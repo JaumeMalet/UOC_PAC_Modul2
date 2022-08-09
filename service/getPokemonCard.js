ce092a48d612e3ced5e655a02dd403753876a062
Jaume <malet.jaume@gmail.com>
2022-08-10 00:18:12 +0200

    Obtenir informació d'un Pokemon.

---
+++ service/getPokemonCard.js
@@ -0,0 +1,15 @@
+export default function getPokemonCard(numPokemon){
+    const apiURL = "https://pokeapi.co/api/v2/pokemon/" + numPokemon;
+    return fetch(apiURL)
+        .then((response) => response.json())
+        .then((data) => {
+            const nom = data.name;
+            const imatge = data.sprites.front_default;
+            const atac = data.stats[1].base_stat;
+            const defensa = data.stats[2].base_stat;
+            return {data, nom, imatge, atac, defensa};
+        })
+        .catch(function(){
+            return "Pokemon number '" + numPokemon + "' does not exists";
+        });    
+}
