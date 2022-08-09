341942f4899a25b6ccc0f2ec708eaee6a7969277
Jaume <malet.jaume@gmail.com>
2022-08-10 00:20:13 +0200

    Obtenir número total de cartes Pokemon existents en PokeAPI

---
+++ service/getPokemonCardsCount.js
@@ -0,0 +1,9 @@
+export default function getPokemonCardsCount(){
+    const apiURL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+    return fetch(apiURL)
+        .then((response) => response.json())
+        .then((data) => {
+            const count = data.count;
+            return {data, count};
+        })
+}
