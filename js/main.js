29bc69e60715008ba8f4ad8306e6e65916e9bbac
Jaume <malet.jaume@gmail.com>
2022-09-10 22:02:32 +0200

    Càrrega de les 10 cartes i carta extesa.

--- js/main.js
+++ js/main.js
@@ -6,8 +6,87 @@ import getPokemonCard from "../service/getPokemonCard.js";
 
 // Definició de constants
 var PokemonCardsCount = await getPokemonCardsCount();
-var PokemonCard = await getPokemonCard(Math.floor(Math.random() * PokemonCardsCount.count));
+// var PokemonCard = await getPokemonCard(Math.floor(Math.random() * PokemonCardsCount.count));
 
-// xDebug
-console.log(PokemonCardsCount);
-console.dir(PokemonCard);   
+const cards = document.querySelectorAll('.card-table');
+
+// consultar si hi ha algun paràmetre a la URL
+let params = new URLSearchParams(document.location.search);
+let id = params.get("pokeID");
+if (id) {
+    // Mostrar carta extesa
+    const cardExt = document.getElementById("card_ext");
+    const cardExtTemplate = document.getElementById("card_ext-template").content;
+    let tipus = "";
+
+    var PokemonCard = await getPokemonCard(id);
+    console.dir(PokemonCard);
+    cardExtTemplate.querySelector('.card_ext-nom').innerHTML = PokemonCard.nom;
+    // Si el link de la imatge frontal és 'null' vincular imatge d'un requadre amb el text 'No image'
+    if(typeof PokemonCard.imatgeFront === 'string') {
+        cardExtTemplate.querySelector('.card_ext-imatge-front').setAttribute('src', PokemonCard.imatgeFront);
+    } else {
+        cardExtTemplate.querySelector('.card_ext-imatge-front').setAttribute('src', "img/void.png");
+    };
+    // Si el link de la imatge posterior és 'null' vincular imatge d'un requadre amb el text 'No image'
+    if(typeof PokemonCard.imatgeBack === 'string') {
+        cardExtTemplate.querySelector('.card_ext-imatge-post').setAttribute('src', PokemonCard.imatgeBack);
+    } else {
+        cardExtTemplate.querySelector('.card_ext-imatge-post').setAttribute('src', "img/void.png");
+    };
+    cardExtTemplate.querySelector('.card_ext-imatge-front').setAttribute('alt', PokemonCard.nom);
+    cardExtTemplate.querySelector('.card_ext-imatge-post').setAttribute('alt', PokemonCard.nom);
+    cardExtTemplate.querySelector('.card_ext-atac').innerHTML = "Atac: " + PokemonCard.atac;
+    cardExtTemplate.querySelector('.card_ext-defensa').innerHTML = "Defensa: " + PokemonCard.defensa;
+    for (let index = 0; index < PokemonCard.tipus.length; index++) {
+        if(index > 0) tipus += ", "
+        tipus += PokemonCard.tipus[index].type.name;
+    }
+    cardExtTemplate.querySelector('.card_ext-tipus').innerHTML = "Tipus: " + tipus;
+    // Afegir la carta a la pàgina
+    cardExt.appendChild(cardExtTemplate.cloneNode(true));
+    
+    // mostrar l'enllaç per tornar enrera
+    back.style.display = "block";
+} else {
+    // Obtenir 10 cartes aleatòriament i sense repetir
+    var aPokemonCard = new Array();
+    const cardTable = document.getElementById("card-table");
+    const cardTemplate = document.getElementById("card-template").content;
+    // const fragment = document.createDocumentFragment(); // Es pot utilitzar com a contenidor intermig alhora de crear les cartes mitjançant la plantilla
+    
+    for (let index = 0; index < 10; index++) {
+        // Obtenir l'informació d'una carta
+        aPokemonCard[index] = await getPokemonCard(Math.floor(Math.random() * PokemonCardsCount.count));
+        // Comprovar si la carta no existeix tornem a repetir aquesta iteració del for
+        if(typeof aPokemonCard[index].atac === 'undefined') {
+            // console.log("undefined");
+            index--;
+            continue;
+        }
+        // Comprovar que no s'ha repetit la carta
+        for (let index2 = 0; index2 < index; index2++) {
+            if(aPokemonCard[index].id == aPokemonCard[index2].id) {
+                // console.log("Repetit: " + aPokemonCard[index].nom);
+                index--;
+                continue;
+            }
+        }
+        // Omplir la plantilla de la carta amb la informació del Pokémon
+        cardTemplate.querySelector('a').attributes['href'].value = "?pokeID=" + aPokemonCard[index].id;
+        cardTemplate.querySelector('.card-nom').innerHTML = aPokemonCard[index].nom;
+        // Si el link de la imatge és 'null' vincular imatge d'un requadre amb el text 'No image'
+        if(typeof aPokemonCard[index].imatgeFront === 'string') {
+            cardTemplate.querySelector('.card-imatge').setAttribute('src', aPokemonCard[index].imatgeFront);
+        } else {
+            cardTemplate.querySelector('.card-imatge').setAttribute('src', "img/void.png");
+        };
+        cardTemplate.querySelector('.card-imatge').setAttribute('alt', aPokemonCard[index].nom);
+        cardTemplate.querySelector('.card-atac').innerHTML = "Atac: " + aPokemonCard[index].atac;
+        cardTemplate.querySelector('.card-defensa').innerHTML = "Defensa: " + aPokemonCard[index].defensa;
+        // Afegir la carta a la pàgina
+        cardTable.appendChild(cardTemplate.cloneNode(true));
+    }
+    // ocultar l'enllaç per tornar enrera
+    back.style.display = "none";
+}
--- service/getPokemonCard.js
+++ service/getPokemonCard.js
@@ -5,10 +5,13 @@ export default function getPokemonCard(numPokemon){
         .then((response) => response.json())
         .then((data) => {
             const nom = data.name;
-            const imatge = data.sprites.front_default;
+            const id = data.id;
+            const imatgeFront = data.sprites.front_default;
+            const imatgeBack = data.sprites.back_default;
             const atac = data.stats[1].base_stat;
             const defensa = data.stats[2].base_stat;
-            return {data, nom, imatge, atac, defensa};
+            const tipus = data.types;
+            return {data, id, nom, imatgeFront, imatgeBack, atac, defensa, tipus};
         })
         .catch(function(){
             return "Pokemon number '" + numPokemon + "' does not exists";
