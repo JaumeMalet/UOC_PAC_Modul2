baf82a0d13b8954fa4115280746cd5853245a8b8
Jaume <malet.jaume@gmail.com>
2022-09-11 21:24:17 +0200

    Funcionalitat de la pàgina combat finalitzada

--- js/combat.js
+++ js/combat.js
@@ -46,3 +46,47 @@ for (let index = 0; index < 10; index++) {
     // Afegir la carta a la pàgina
     cardTable.appendChild(cardTemplate.cloneNode(true));
 }
+
+
+// Afegir funcionalitat de girar la carta al fer-hi click
+var flip_cards = document.querySelectorAll('.flip-card');
+var flipped_cards_index = 0;
+var flipped_card_val = new Object();
+var flipped_cards_array = new Array();
+
+flip_cards.forEach((flip_card) => {
+    flip_card.addEventListener('click', function() {
+        flip_card.classList.add('flipped');
+        flipped_card_val =
+        {
+            "card_nom": flip_card.querySelector('.card-nom').textContent,
+            "card_atac": (flip_card.querySelector('.card-atac').textContent).split(" ")[1],
+            "card_defensa": (flip_card.querySelector('.card-defensa').textContent).split(" ")[1]
+        }
+        flip_card_func(flipped_card_val); 
+    });
+});
+
+
+// Comprovar si ja hi ha 2 cartes girades per començar el combat
+function flip_card_func(obj) {
+    flipped_cards_index++; // Index de la carta girada
+    flipped_cards_array[flipped_cards_index] = JSON.parse(JSON.stringify(obj)); // Guardar valors carta girada
+
+    if(flipped_cards_index == 2) {
+        // Tenim 2 cartes girades, començar el combat:
+        setTimeout(function() { // Executar funció després d'un retard de 0.5s per deixar temps perquè es gira la segona carta.
+            // Missatge resultat del combat:
+            if(Number(flipped_cards_array[1].card_atac) > Number(flipped_cards_array[2].card_defensa)) {
+                alert("'" + flipped_cards_array[1].card_nom + "' ataca i guanya a '" + flipped_cards_array[2].card_nom + "'");
+            }
+            else {
+                alert("'" + flipped_cards_array[1].card_nom + "' ataca i perd contra '" + flipped_cards_array[2].card_nom + "'");
+            }
+            // Recarregar la pàgina de combat
+            window.location.reload();
+        }, 500)
+    }
+}
+
+
